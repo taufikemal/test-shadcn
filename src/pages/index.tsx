f77a080988b54bd1e0f971135965b023c767da99
taufikemal <artcrev7@gmail.com>
2024-09-21 18:53:18 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import localFont from "next/font/local";
+import { Button } from "@/components/ui/button"
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[24px_1fr_24px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Button>Submit</Button>
+      <Button variant={"secondary"}>Submit</Button>
+      <Button variant={"ghost"}>Submit</Button>
+    </div>
+  );
+}
