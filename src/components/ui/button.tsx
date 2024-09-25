0ee6a52ba7e7c596b177c1110152a12dc928ae4a
taufikemal <artcrev7@gmail.com>
2024-09-25 14:59:43 +0700

    add font

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,26 +9,35 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: 
+          "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        ghost: 
+          "hover:bg-accent hover:text-accent-foreground",
+        link: 
+          "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: 
+          "h-10 px-4 py-2",
+        sm: 
+          "h-9 rounded-md px-3",
+        lg: 
+          "h-11 rounded-md px-8",
+        icon: 
+          "h-10 w-10",
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 
+        "default",
+      size: 
+        "default",
     },
   }
 )
@@ -51,6 +60,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   }
 )
-Button.displayName = "Button"
+Button.displayName = "Button Preview"
 
 export { Button, buttonVariants }
